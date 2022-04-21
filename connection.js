bb524c6e03c55119b8093229015edc076fe17326
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-04-21 22:53:04 +0200

    Add connection credentials in ignored file

--- connection.js
+++ connection.js
@@ -1,8 +1,19 @@
 const mysql = require('mysql')
+
+/* skapa din egen credentials.js med innehåll:
+
+module.exports = {
+  password: 'ditt-databas-lösenord'
+}
+
+credentials.js är i .gitignore för att undvika att pusha lösenord till github:)
+*/
+const credentials = require('./credentials')
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'rootuser',
-  password: '',
+  password: credentials.password,
   database: 'moviesProject'
 })
 
