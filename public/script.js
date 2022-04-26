78b7f60b4164a7d42d0dc0d73866a5efc36ed84d
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-04-26 12:48:25 +0200

    fetch for sql and nosql

--- public/script.js
+++ public/script.js
@@ -1,71 +1,92 @@
-//fatstack påbörjade directors.
-const getDirectors = () => {
-  fetch('http://localhost:1337/directors')
+// SQL get all movies with movieTitle genreType actorName directorName streamingAppTitle movieReleaseYear
+const getAllMovies = () => {
+  fetch('http://localhost:1337/allmovies')
     .then((response) => response.json())
     .then((result) => {
-      console.log('Directors:', result)
-      const directors = result
-
-      directors.forEach((director) => {
-        const directorList = document.getElementById('director-list')
-
-        const directorElement = document.createElement('div')
-        directorElement.className = 'card'
-
-        const directorElementBody = document.createElement('div')
-        directorElementBody.className = 'card-body'
-
-        const directorName = document.createElement('div')
-        directorName.className = 'title'
-        directorName.innerHTML = 'Director: ' + director.directorName
-
-        directorList.appendChild(directorElement)
-        directorElement.appendChild(directorElementBody)
-        directorElementBody.appendChild(directorName)
-      })
+      console.log('All movies: ', result)
     })
 }
-getDirectors()
+getAllMovies()
 
-// --------------------------------------------------------
+// -------------------------------
 
-function getMovies() {
-  let promise = fetch('http://localhost:1337/movie')
-
-  return promise.then((response) => response.json())
+// NoSQL
+const getAllReviews = () => {
+  fetch('http://localhost:1337/movieReviews')
+    .then((response) => response.json())
+    .then((result) => {
+      console.log('All reviews: ', result)
+    })
 }
+getAllReviews()
+// ------------------
+// const getDirectors = () => {
+//   fetch('http://localhost:1337/directors')
+//     .then((response) => response.json())
+//     .then((result) => {
+//       console.log('Directors:', result)
+//       const directors = result
 
-async function main() {
-  movies = await getMovies()
-  renderMovies(movies)
-  console.log('Movies:', movies)
-}
+//       directors.forEach((director) => {
+//         const directorList = document.getElementById('director-list')
 
-main()
+//         const directorElement = document.createElement('div')
+//         directorElement.className = 'card'
 
-function renderMovies(movies) {
-  const movieList = document.querySelector('#movie-list')
+//         const directorElementBody = document.createElement('div')
+//         directorElementBody.className = 'card-body'
 
-  movieList.innerHTML = ''
+//         const directorName = document.createElement('div')
+//         directorName.className = 'title'
+//         directorName.innerHTML = 'Director: ' + director.directorName
 
-  for (const movie of movies) {
-    const movieElement = document.createElement('div')
-    movieElement.className = 'card'
+//         directorList.appendChild(directorElement)
+//         directorElement.appendChild(directorElementBody)
+//         directorElementBody.appendChild(directorName)
+//       })
+//     })
+// }
+// getDirectors()
 
-    const movieElementBody = document.createElement('div')
-    movieElementBody.className = 'card-body'
+// // --------------------------------------------------------
 
-    const movieTitle = document.createElement('div')
-    movieTitle.className = 'title'
+// function getMovies() {
+//   let promise = fetch('http://localhost:1337/movie')
 
-    const movieYear = document.createElement('p')
+//   return promise.then((response) => response.json())
+// }
 
-    movieTitle.innerText = movie.movieTitle
-    movieYear.innerText = movie.movieReleaseYear
+// async function main() {
+//   movies = await getMovies()
+//   renderMovies(movies)
+//   console.log('Movies:', movies)
+// }
 
-    movieList.appendChild(movieElement)
-    movieElement.appendChild(movieElementBody)
-    movieElementBody.appendChild(movieTitle)
-    movieElementBody.appendChild(movieYear)
-  }
-}
+// main()
+
+// function renderMovies(movies) {
+//   const movieList = document.querySelector('#movie-list')
+
+//   movieList.innerHTML = ''
+
+//   for (const movie of movies) {
+//     const movieElement = document.createElement('div')
+//     movieElement.className = 'card'
+
+//     const movieElementBody = document.createElement('div')
+//     movieElementBody.className = 'card-body'
+
+//     const movieTitle = document.createElement('div')
+//     movieTitle.className = 'title'
+
+//     const movieYear = document.createElement('p')
+
+//     movieTitle.innerText = movie.movieTitle
+//     movieYear.innerText = movie.movieReleaseYear
+
+//     movieList.appendChild(movieElement)
+//     movieElement.appendChild(movieElementBody)
+//     movieElementBody.appendChild(movieTitle)
+//     movieElementBody.appendChild(movieYear)
+//   }
+// }
