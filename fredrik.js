ca07e350f256c9e4347254f95065728887d0f12d
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-04-26 10:49:50 +0200

    inner joins
    mahdi stf up it works broo

--- fredrik.js
+++ fredrik.js
@@ -42,6 +42,17 @@ app.get('/movies', (req, res) => {
     res.json(results)
   })
 })
+
+//GET ALL MOVIES WITH CHOOSEN TABLES
+app.get('/allmovies', (req, res) => {
+  let sql =
+    'SELECT movie.movieTitle, genre.genreType,actor.actorName,director.directorName,streamingApp.streamingAppTitle FROM genre INNER JOIN movie ON genre.genreId = movie.movieGenreId INNER JOIN actorMovie ON movie.movieId = actorMovie.actorMovieMId INNER JOIN actor ON actorMovie.actorMovieAId = actor.actorId INNER JOIN director ON movie.movieDirectorId = director.directorId INNER JOIN streamingAppMovie ON movie.movieId = streamingAppMovie.streamingAppMovieMId INNER JOIN streamingApp ON streamingApp.streamingAppId = streamingAppMovie.streamingAppMovieSId'
+  connection.query(sql, (err, results) => {
+    if (err) throw err
+    res.json(results)
+  })
+})
+
 // POST funkar men movieTitle skrivs ut konstigt = "\"Taxi Driver\""
 app.post('/movies', (req, res) => {
   let sql =
