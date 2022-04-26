d5f61ecf62bad8a7c3a525b74b0e7662ec71647b
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-04-26 20:49:55 +0200

    done with frontend for movies

--- public/script.js
+++ public/script.js
@@ -16,19 +16,31 @@ const getAllMovies = () => {
         movieElementBody.className = 'card-body'
 
         const movieUl = document.createElement('ul')
-        const movieTitle = document.createElement('li')
-        const genreType = document.createElement('li')
-        const actorName = document.createElement('li')
-        const directorName = document.createElement('li')
-        const streamingAppTitle = document.createElement('li')
-        const movieReleaseYear = document.createElement('li')
 
-        movieTitle.innerHTML = 'Title: ' + movies.movieTitle
-        genreType.innerHTML = 'Genre: ' + movies.genreType
-        actorName.innerHTML = 'Actor: ' + movies.actorName
-        directorName.innerHTML = 'Director: ' + movies.directorName
-        streamingAppTitle.innerHTML = 'Stream at: ' + movies.streamingAppTitle
-        movieReleaseYear.innerHTML = 'Release: ' + movies.movieReleaseYear
+        const movieTitle = document.createElement('li')
+        movieTitle.className = 'movieTitleStyle'
+
+        const genreType = document.createElement('li')
+        genreType.className = 'genreTypeStyle'
+
+        const actorName = document.createElement('li')
+        actorName.className = 'actorNameStyle'
+
+        const directorName = document.createElement('li')
+        directorName.className = 'directorNameStyle'
+
+        const streamingAppTitle = document.createElement('li')
+        streamingAppTitle.className = 'streamingNameStyle'
+
+        const movieReleaseYear = document.createElement('li')
+        movieReleaseYear.className = 'releaseYearStyle'
+
+        movieTitle.innerHTML = ' ' + movies.movieTitle
+        genreType.innerHTML = ' ' + movies.genreType
+        actorName.innerHTML = ' ' + movies.actorName
+        directorName.innerHTML = ' ' + movies.directorName
+        streamingAppTitle.innerHTML = ' ' + movies.streamingAppTitle
+        movieReleaseYear.innerHTML = ' ' + movies.movieReleaseYear
 
         movieList.appendChild(movieElement)
         movieElement.appendChild(movieElementBody)
@@ -102,29 +114,75 @@ getAllReviews()
 
 // main()
 
-function renderMovies(movies) {
-  const movieList = document.querySelector('#movie-list')
+// function renderMovies(movies) {
+//   const movieList = document.querySelector('#movie-list')
 
-  movieList.innerHTML = ''
+//   movieList.innerHTML = ''
 
-  for (const movie of movies) {
-    const movieElement = document.createElement('div')
-    movieElement.className = 'card'
+//   for (const movie of movies) {
+//     const movieElement = document.createElement('div')
+//     movieElement.className = 'card'
 
-    const movieElementBody = document.createElement('div')
-    movieElementBody.className = 'card-body'
+//     const movieElementBody = document.createElement('div')
+//     movieElementBody.className = 'card-body'
 
-    const movieTitle = document.createElement('div')
-    movieTitle.className = 'title'
+//     const movieTitle = document.createElement('div')
+//     movieTitle.className = 'title'
 
-    const movieYear = document.createElement('p')
+//     const movieYear = document.createElement('p')
 
-    movieTitle.innerText = movie.movieTitle
-    movieYear.innerText = movie.movieReleaseYear
+//     movieTitle.innerText = movie.movieTitle
+//     movieYear.innerText = movie.movieReleaseYear
 
-    movieList.appendChild(movieElement)
-    movieElement.appendChild(movieElementBody)
-    movieElementBody.appendChild(movieTitle)
-    movieElementBody.appendChild(movieYear)
-  }
-}
+//     movieList.appendChild(movieElement)
+//     movieElement.appendChild(movieElementBody)
+//     movieElementBody.appendChild(movieTitle)
+//     movieElementBody.appendChild(movieYear)
+//   }
+// }
+
+// const getAllMovies = () => {
+//   fetch('http://localhost:1337/allmovies')
+//     .then((response) => response.json())
+//     .then((result) => {
+//       console.log('All movies: ', result)
+//       const allMovies = result
+
+//       allMovies.forEach((movies) => {
+//         const movieList = document.getElementById('movie-list')
+
+//         const movieElement = document.createElement('div')
+//         movieElement.className = 'card'
+
+//         const movieElementBody = document.createElement('div')
+//         movieElementBody.className = 'card-body'
+
+//         const movieUl = document.createElement('ul')
+//         const movieTitle = document.createElement('li')
+//         movieTitle.style.color = '#3195ff'
+//         const genreType = document.createElement('li')
+//         const actorName = document.createElement('li')
+//         const directorName = document.createElement('li')
+//         const streamingAppTitle = document.createElement('li')
+//         const movieReleaseYear = document.createElement('li')
+
+//         movieTitle.innerHTML = 'Title: ' + movies.movieTitle
+//         genreType.innerHTML = 'Genre: ' + movies.genreType
+//         actorName.innerHTML = 'Actor: ' + movies.actorName
+//         directorName.innerHTML = 'Director: ' + movies.directorName
+//         streamingAppTitle.innerHTML = 'Stream at: ' + movies.streamingAppTitle
+//         movieReleaseYear.innerHTML = 'Release: ' + movies.movieReleaseYear
+
+//         movieList.appendChild(movieElement)
+//         movieElement.appendChild(movieElementBody)
+//         movieUl.appendChild(movieTitle)
+//         movieUl.appendChild(genreType)
+//         movieUl.appendChild(actorName)
+//         movieUl.appendChild(directorName)
+//         movieUl.appendChild(streamingAppTitle)
+//         movieUl.appendChild(movieReleaseYear)
+//         movieElementBody.appendChild(movieUl)
+//       })
+//     })
+// }
+// getAllMovies()
