416b1a2526e81745831a70cb04f517d74b6d71ff
Pernilla Skeppstedt <pernilla.skeppstedt@gmail.com>
2022-04-27 18:43:42 +0200

    deleted test files

--- fetch-movie-post.js
+++ fetch-movie-post.js
@@ -15,28 +15,28 @@ function newMovie(event) {
   let movieGenreId = elementGenreId.value;
   let movieDirectorId = elementDirectorId.value;
 
-  async function postData(url = "http://localhost:1337/movies", data = {}) {
+  async function postData(url = 'http://localhost:1337/movies', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
-      method: "POST", // GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors,cors, same-origin
-      cache: "no-cache", // default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include,same-origin, omit
+      method: 'POST', // GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors,cors, same-origin
+      cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include,same-origin, omit
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: "follow", // manual, follow, error
-      referrerPolicy: "no-referrer", // no-referrer,client
+      redirect: 'follow', // manual, follow, error
+      referrerPolicy: 'no-referrer', // no-referrer,client
       body: JSON.stringify({
         movieTitle: movieTitle,
         movieReleaseYear: movieReleaseYear,
         movieActorId: movieActorId,
         movieGenreId: movieGenreId,
-        movieDirectorId: movieDirectorId,
-      }), // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+        movieDirectorId: movieDirectorId
+      }) // body data type must match "Content-Type" header
+    })
+    return response.json() // parses JSON response into native JavaScript objects
   }
 
   postData("http://localhost:1337/movies").then((data) => {
